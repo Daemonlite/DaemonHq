9e62bb108f48bfdf32fb896c78c117b4281c80e7
isaaccode246 <uzoigweisaac246@gmail.com>
2023-03-29 22:58:18 +0100

    My branch

--- startUp/client/src/pages/Login.jsx
+++ startUp/client/src/pages/Login.jsx
@@ -1,57 +1,55 @@
 import { Button } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import FormControl from '@mui/material/FormControl';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import GoogleIcon from '@mui/icons-material/Google';
-import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import {useState } from 'react'
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import axios from 'axios'
-import {toast} from 'react-toastify'
-import {useNavigate} from 'react-router-dom'
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import GoogleIcon from "@mui/icons-material/Google";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const [email,setEmail] = useState('')
-    const [password,setpassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
 
-    const navigate = useNavigate()
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      axios
-        .post('http://localhost:7000/api/users/login/', {
-          email,
-          password,
-        })
-        .then((res) => {
-  
-          toast.success('login successful');
-          if (res.data) {
-            localStorage.setItem('userInfo', JSON.stringify(res.data));
-            navigate('/home');
-          }
-          setpassword('');
-          setEmail('');
-        })
-        .catch((err) => {
-          toast.error(err.response.data);
-        });
-    }
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-  
-    const handleMouseDownPassword = (event) => {
-      event.preventDefault();
-    };
-    
-    return(
-        <div>
- <div className="land">
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:7000/api/users/login/", {
+        email,
+        password,
+      })
+      .then((res) => {
+        toast.success("login successful");
+        if (res.data) {
+          localStorage.setItem("userInfo", JSON.stringify(res.data));
+          navigate("/home");
+        }
+        setpassword("");
+        setEmail("");
+      })
+      .catch((err) => {
+        toast.error(err.response.data);
+      });
+  };
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <div className="land">
         <header>
-          <h1 className="head">Welcome to a 
-          professional community</h1>
+          <h1 className="head">Welcome to a professional community</h1>
           <Box
             component="form"
             sx={{
@@ -60,58 +58,62 @@ const Login = () => {
             noValidate
             autoComplete="off"
             onSubmit={handleSubmit}
-            className='login'
+            className="login"
           >
             <TextField
               id="outlined-basic"
               label="Enter Email"
               variant="outlined"
               type="email"
-              onChange={(e)=>setEmail(e.target.value)}
-              className='field'
+              onChange={(e) => setEmail(e.target.value)}
+              className="field"
             />
             <br />
             <br />
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            onChange={(e)=>setpassword(e.target.value)}
-            className='field'
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-            
-          />
-        </FormControl>
+            <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                onChange={(e) => setpassword(e.target.value)}
+                className="field"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
             <br />
             <a href="/reset">forgot password?</a>
             <br />
-            <Button variant="contained" style={{ width: "435px" }} type='submit'>
+            <Button
+              variant="contained"
+              style={{ width: "435px" }}
+              type="submit"
+            >
               sign In
             </Button>
             <br />
             <p style={{ textAlign: "center" }}>or</p>
             <br />
             <div className="google">
-             <GoogleIcon/>
+              <GoogleIcon />
               Sign in with google
             </div>
             <a href="/register" className="google">
               New to Daemonhq? signUp
             </a>
-            
           </Box>
         </header>
         <div className="head-img">
@@ -122,8 +124,8 @@ const Login = () => {
           />
         </div>
       </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
