4bac128f35860fe60552f2bdb72b575a125ed294
eugene <paakwesinunoo135@gmail.com>
2023-03-31 10:28:07 +0100

    starting backend with socket.io

--- startUp/server/Handlers/bidHandler.js
+++ startUp/server/Handlers/bidHandler.js
@@ -1,112 +1,106 @@
 const Bid = require('../Models/bidModel')
 const Listing = require('../Models/listingModel')
 
-const getBids = async (req,res) => {
-    try {
-        const bid = await Bid.find();
-        return res.status(200).json(bid);
-      } catch (error) {
-        console.error(error);
-        return res
-          .status(500)
-          .json({ message: "Failed to retrieve Bids" });
-      }
-}
+const getBids = async (req, res) => {
+  try {
+    const bids = await Bid.find();
+    res.status(200).json(bids);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to retrieve bids" });
+  }
+};
 
-const getBidsById = async (req,res) => {
+const getBidById = async (req, res) => {
   const { id } = req.params;
   try {
     const bid = await Bid.findById(id);
     if (!bid) {
-      return res.status(404).json({ message: "biding not found" });
+      return res.status(404).json({ message: "Bid not found" });
     }
-    return res.status(200).json(bid);
+    res.status(200).json(bid);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Failed to retrieve Bids" });
+    res.status(500).json({ message: "Failed to retrieve bid" });
   }
-}
-const placeBid = async (req,res) => {
-    const {userName,userProfile,stake,listing,isAccepted} = req.body
-      
-    if (!userName || !userProfile || !stake){
-        return res.status(400).json({message:"Please add required fields"})
-    }
+};
 
-    let existingListing;
-    try {
-     existingListing = await Listing.findById(listing);
-    } catch (error) {
-      console.log(error);
-    }
+const placeBid = async (req, res) => {
+  const { userName, userProfile, stake, listing, isAccepted } = req.body;
+
+  if (!userName || !userProfile || !stake) {
+    return res.status(400).json({ message: "Please add required fields" });
+  }
+
+  try {
+    const existingListing = await Listing.findById(listing);
     if (!existingListing) {
       return res.status(400).json({ message: "Listing not found" });
     }
 
     const bid = new Bid({
-        userProfile,
-        userName,
-        stake,
-        listing,
-        isAccepted
-    })
+      userProfile,
+      userName,
+      stake,
+      listing,
+      isAccepted,
+    });
 
-     try {
-        await bid.save()
-        existingListing.bids.unshift(bid);
-        await existingListing.save();
-     } catch (error) {
-        console.log(error)
-        res.status(400).json({ error: error });
-     }
+    await bid.save();
+    existingListing.bids.unshift(bid);
+    await existingListing.save();
 
-     return res.status(201).json(bid);
-}
+    return res.status(201).json(bid);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Failed to place bid" });
+  }
+};
 
-const updateBid = async (req,res) => {
-  const compId = req.params.id;
+const updateBid = async (req, res) => {
+  const bidId = req.params.id;
   const updatedInfo = req.body;
 
-  const upbidInfo = await Bid.findByIdAndUpdate(compId, updatedInfo, {
-    new: true,
-  });
-
-  if (!upbidInfo) {
-    return res.status(404).json({ message: "bid not found" });
-  }
-
-  res.status(200).json(upbidInfo);
-}
-const withdrawBid = async (req,res) => {
-
- let bid;
-    try {
-      bid = await Bid.findOne({ _id: id });
-      if (!bid) {
-        return res
-          .status(404)
-          .json({ message: "The specified bid was not found." });
-      }
-      await bid.deleteOne({ _id: id });
-      await Listing.updateOne(
-        { _id: bid.listing },
-        { $pull: { bids: { _id: bid._id } } }
-      );
-    } catch (err) {
-      return res.status(500).json({
-        message:
-          "Unable to withdraw the  bid. An internal server error has occurred.",
-      });
+  try {
+    const updatedBid = await Bid.findByIdAndUpdate(bidId, updatedInfo, {
+      new: true,
+    });
+    if (!updatedBid) {
+      return res.status(404).json({ message: "Bid not found" });
     }
+    return res.status(200).json(updatedBid);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Failed to update bid" });
+  }
+};
+
+const withdrawBid = async (req, res) => {
+  const bidId = req.params.id;
+
+  try {
+    const bid = await Bid.findById(bidId);
+    if (!bid) {
+      return res.status(404).json({ message: "Bid not found" });
+    }
+    const deletedBid = await bid.deleteOne();
+    await Listing.findByIdAndUpdate(bid.listing, {
+      $pull: { bids: bidId },
+    });
+    return res.status(200).json({ message: "Bid withdrawn successfully" });
+  } catch (error) {
+    console.error(error);
     return res
-      .status(200)
-      .json({ message: "Bid withdrawn  successfully" });
-}
+      .status(500)
+      .json({ message: "Failed to withdraw the bid" });
+  }
+};
+
 
 module.exports = {
     getBids,
     placeBid,
     withdrawBid,
-    getBidsById,
+    getBidById,
     updateBid
 }
